d8a5536d16bac9cbc4cd2c2e5a37ad6609867dc3
Diego-MV <diegomoralesval@gmail.com>
2021-07-04 19:01:31 -0400

    connection test3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import socket
 
-#####CONNECTION#######
+#-------CONNECTION-------#
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 SERVER = '200.14.84.235'
 PORT = 5000
 print(f'starting up on {SERVER} port {PORT}')
 socket.connect((SERVER, PORT))
-####################
+#------------------------#
+
 
