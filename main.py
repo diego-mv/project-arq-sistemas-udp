e2ddd80be0db0c6a4c247ad36b3f77c9103bddbf
ee5776 <diego.moralesvaldivia@externos-cl.cencosud.com>
2021-07-04 18:51:10 -0400

    connection test

---
+++ main.py
@@ -0,0 +1,10 @@
+import socket
+
+#####CONECTION#######
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER = '200.14.84.235'
+PORT = 5000
+print(f'starting up on {SERVER} port {PORT}')
+socket.connect((SERVER, PORT))
+####################
+
