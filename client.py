ab2026638b8539a44f5977bc6df5daae1e5e7e78
Diego-MV <diegomoralesval@gmail.com>
2021-07-05 03:09:48 -0400

    avance cliente leer info

--- client.py
+++ client.py
@@ -30,32 +30,167 @@ def GetFromService(name_service):
     print(status)
     return status
 
+rut_usuario = ''
+HORARIOS = [
+'09:00 - 10:00',
+'10:00 - 11:00',
+'11:00 - 12:00',
+'12:00 - 13:00',
+'13:00 - 14:00',
+'14:00 - 15:00',
+'15:00 - 16:00',
+'16:00 - 17:00'
+]
+
 print('•• ¡Bienvenido al sistema de reserva de espacios! ••')
 while True:
+    print('Que desea hacer:')
     print('1. Iniciar Sesión')
     print('2. Registro')
     print('3. Salir')
     opt = int(input("\n>> "))
     
     if(opt == 1):
-        if(GetFromService('login-sist-reserva')):
+        if(GetFromService('login_sist_reserva')) == 'OK':
             print('Ingrese sus credenciales:')
             rut = input('RUT: ')
             psw = password = getpass('Contraseña: ')
-            data = { 
+            data_login = { 
                 'rut': rut,
                 'password': psw
             }
-            SendToService('login-sist-reserva', data)
+            SendToService('login_sist_reserva', data_login)
             
             while True: 
                 data_service = socket.recv(390)
-                print(data_service)
                 break
-            print(data_service)
+
+            if (True): #falta condicion de logged true IF LOGGIN == TRUE
+                rut_usuario = data_login['rut']
+                print('Inicio de sesión exitoso.')
+                #---------------------------------MENU DESPUES DE LOGGEO-----------------------------------#
+                print('Qué desea hacer:')
+                print('1. Realizar reserva de sala')
+                print('2. Cancelar reserva')
+                print('3. Salir')
+                opt2 = int(input('>> '))
+                if(opt2 == 1):
+                    if GetFromService('lista_de_salas') == 'OK':
+                        data_list_sala = { 
+                            'rut' : rut_usuario
+                        }
+                        SendToService('lista_de_salas', data_list_sala)
+                        
+                        while True: 
+                            data_service_salas = socket.recv(390)
+                            break
+                        #--------------------------------------MENU SALAS DISPONIBLES----------------------------------#
+                        print('Salas disponibles:')
+                        for i in len(data_service_salas):
+                            print(f'{i+1}. {data_service_salas[1]} | Aforo permitido: {data_service_salas[2]}')
+                        
+                        opt_sala = int(input('>> '))
+                        fecha_req = input('Ingrese la fecha que requiere una reserva (dd-mm-yyyy): \n>> ') #------------------------!!!!!!!!!!!!!!!!VALIDAR FECHA CON EXPRESION REGULAR dd/mm/yyyy!!!!!!!!!!!!!!!
+
+                        data_sala = {
+                            'id_sala': data_service_salas[opt_sala-1][0],
+                            'fecha_req': fecha_req    
+                        }
+                        if GetFromService('horarios_usados_sala') == 'OK':
+                            SendToService('horarios_usados_sala', data_sala)
+
+                            while True: 
+                                data_service_horario_usado = socket.recv(390)
+                                break
+                            #-------------------------------------HORARIOS DISPONIBLES DE UNA SALA EN UN DIA ESPECIFICOS-------------------------#
+                            HORARIOS_DISP = []
+                            print(f'Horarios disponibles en el día {fecha_req}')
+                            for i in len(HORARIOS):
+                                if(data_service_horario_usado != HORARIOS[i]):
+                                    HORARIOS_DISP.append(HORARIOS[i])
+                                    print(f'{i+1}. {HORARIOS[i]}')
+                            
+                            opt_horario = int(input('>> '))
+                            
+                            menu_conf_reserva = True
+                            PARTICIPANTES = []
+                            while menu_conf_reserva: 
+                                #=-------------------------------------------------MENU CONFIRMACION DE RESERVA-----------------------------------------------------#
+                                print('Que desea hacer:')
+                                print('1. Agregar participante a la reunion')
+                                print('2. Confirmar reserva (no puede ingresar más participantes)')
+                                print('3. No realizar reserva y salir')
+                                opt3 = int(input('>> '))
+                                
+                                if(opt3 == 1): #Ingresar participante
+                                    print('Ingrese datos del participante:')
+                                    rut_p = input('RUT: ')
+                                    nombre_p = input('Nombre: ')
+                                    correo_p = input('Email: ')
+                                    
+                                    if not rut_p or not nombre_p or not correo_p:
+                                        print('No se pudo ingresar al participante porque un campo no es correcto.')
+                                    else:
+                                        PARTICIPANTES.append({
+                                            'rut': rut_p,
+                                            'nombre': nombre_p,
+                                            'correo': correo_p,
+                                            'reserva_id': data_service_salas[opt_sala-1][0]
+                                        })
+                                        print('Participante agregado')
+                                elif(opt3 == 2): #Confirmar reserva
+                                    if GetFromService('confirmar_reserva') == 'OK' and GetFromService('agregar_participante_en_reserva') == 'OK':
+                                        print('Confirmando reserva...')
+                                        data_confirma_reserva = {
+                                            'inicia': f'{fecha_req} {HORARIOS_DISP[opt_horario][:5]}',
+                                            'termina': f'{fecha_req} {HORARIOS_DISP[opt_horario][8:]}',
+                                            'anfitrion_rut': rut_usuario,
+                                            'sala_id': data_service_salas[opt_sala-1][0]
+                                        }
+                                        SendToService('confirmar_reserva', data_confirma_reserva)
+
+                                        while True: 
+                                            data_service_horario_usado = socket.recv(390)
+                                            print(data_service)
+                                            break
+                                        #AGREGA A LOS PARTICIPANTES EN LA BD DESPUES DE AGREGAR LA RESERVA EN LA BD
+                                        data_participante = {
+                                            'rut': rut_p,
+                                            'nombre': nombre_p,
+                                            'correo': correo_p,
+                                            'reserva_id': data_service_salas[opt_sala-1][0]
+                                        }
+                                        SendToService('agregar_participante_en_reserva', data_participante)
+
+                                        while True: 
+                                            data_service_horario_usado = socket.recv(390)
+                                            break
+
+                                        print('Reserva realizada, ¡adios!')
+                                        menu_conf_reserva = False
+                                    else: 
+                                        print('Servicio de reserva no disponible')  
+                                        menu_conf_reserva = False
+                                else:
+                                    print('¡ADIOS!')
+                                    menu_conf_reserva = False
+                        else: 
+                            print('Servicio de reserva no disponible')    
+                    else: 
+                        print('Servicio de reserva no disponible')
+
+                elif(opt2 == 2): #Cancela reserva ya guardada en la BD
+                    print('Indique la reserva que desea cancelar:')
+                else:
+                    print('¡ADIOS!')
+                    pass
+
+            else:
+                print('No se pudo iniciar sesión, intente nuevamente.')
+                pass
 #--------------------------Registro--------------------------#
     elif(opt == 2):
-        if(GetFromService('registro-sist-reserva')):
+        if(GetFromService('registro_sist_reserva')) == 'OK':
             print('Ingrese los datos para su cuenta:')
             rut = input('RUT: ')
             name = input('Nombre: ')
@@ -70,13 +205,15 @@ while True:
                 'fono': fono,
                 'password': psw
             }
-            SendToService('registro-sist-reserva', data)
+            SendToService('registro_sist_reserva', data)
             
             while True: 
                 data_service = socket.recv(390)
                 print(data_service)
                 break
             print(data_service)
+
+            #if register is success else register is error ......
 
     elif(opt == 3):
         print('¡Hasta luego!') 
--- create_db.py
+++ create_db.py
@@ -4,23 +4,25 @@ conn_bd = sqlite3.connect('project-arq-sist.db')
 cur = conn_bd.cursor()
 
 cur.execute('CREATE TABLE Rol(id int PRIMARY KEY, nombre varchar(50));')
-cur.fetchall()
 
 cur.execute('CREATE TABLE Usuario(rut int PRIMARY KEY, nombre varchar(100), correo varchar(100), fono int, pwhash longtext, rol_id int, FOREIGN KEY(rol_id) REFERENCES Rol(id))')
-cur.fetchall()
 
 cur.execute('CREATE TABLE Sala(id int PRIMARY KEY, ubicacion varchar(100), aforo int);')
-cur.fetchall()
 
 cur.execute('CREATE TABLE EstadoReserva(id int PRIMARY KEY, nombre varchar(50));')
-cur.fetchall()
 
 cur.execute('CREATE TABLE Reserva(id int PRIMARY KEY, inicia varchar(50), termina varchar(50), anfitrion_id int, sala_id int, estado_id int, FOREIGN KEY(anfitrion_id) REFERENCES Usuario(id),FOREIGN KEY(sala_id) REFERENCES Sala(id), FOREIGN KEY(estado_id) REFERENCES EstadoReserva(id))')
-cur.fetchall()
 
 cur.execute('CREATE TABLE Invitados(rut int PRIMARY KEY, nombre varchar(100), correo varchar(100), asistio int, reserva_id int, FOREIGN KEY(reserva_id) REFERENCES Reserva(id))')
-cur.fetchall()
 
+Roles = [(1, "Admin"), (2, "Usuario")]
+cur.executemany("INSERT INTO Rol VALUES (?, ?);", Roles)
+
+Salas = [(1, "Piso 1, Sala 12", 10), (2, "Piso 1, Sala 11", 15), (3, "Piso 2, Sala 24", 8)]
+cur.executemany("INSERT INTO Rol Sala (?, ?, ?);", Salas)
+
+Estados = [(1, "Reserva realizada"), (2, "Reserva Cancelada"), (2, "Reserva Terminada")]
+cur.executemany("INSERT INTO Rol EstadoReserva (?, ?);", Estados)
 
 conn_bd.commit()
 
